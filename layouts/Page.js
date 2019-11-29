e2c6611fa8a57adb371adbbcea3da837909328c1
Jorge Donoso <jdevcl@gmail.com>
2019-11-29 14:09:33 -0500

    Add page title as prop

--- layouts/Page.js
+++ layouts/Page.js
@@ -1,13 +1,20 @@
 import Navigation from "../components/Navigation"
 import Head from 'next/head'
+import "bootstrap-css-only/css/bootstrap.min.css"
 
-export default ({ children }) => (
+export default ({ children, title }) => (
     <>
         <Head>
             <title>Reactive Poker</title>
             <link rel="icon" href="/favicon.ico" />
+            <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         </Head>
         <Navigation></Navigation>
-        {children}
+        <div className="container">
+            <h1 className="display-1 text-center my-4">{title}</h1>
+            <div className="row">
+                {children}
+            </div>
+        </div>
     </>
 )
