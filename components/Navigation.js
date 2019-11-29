1ea543ce30bf7fe1068d0777479422a392c61418
Jorge Donoso <jdevcl@gmail.com>
2019-11-29 14:04:01 -0500

    Add base navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,19 +3,21 @@ import Link from "next/link"
 const links = [
     { label: "Home", path: "/" },
     { label: "Poker Rules", path: "/poker-rules" },
-    { label: "Card Counting", path: "/card-counting" },
     { label: "Jacks or Better", path: "/jacks-or-better" },
-    { label: "Terms of Service", path: "/terms-of-service" },
+    { label: "Card Counting", path: "/card-counting" },
+    { label: "Terms of Service", path: "/terms-of-service" }
 ]
 
-const Navigation = () => (
-    <ul>
-        {links.map(
-            (link, $key) => <li>
-                <Link key={$key} href={link.path}><a>{link.label}</a></Link>
-            </li>
-        )}
-    </ul>
-)
+const Navigation = () =>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <Link href="/"><a className="navbar-brand">RP</a></Link>
+        <div className="navbar-nav">
+            {links.map(
+                (link, $key) =>
+                    <Link key={$key} href={link.path}><a className="nav-item nav-link">{link.label}</a></Link>
+            )}
+        </div>
+    </nav>
+
 
 export default Navigation
