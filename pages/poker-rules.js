fee691c8b4cc843d9df9dba8459679a03e7c4bd8
Jorge Donoso <jdevcl@gmail.com>
2019-11-29 20:54:31 -0500

    Show random hand

--- pages/poker-rules.js
+++ pages/poker-rules.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Page from "../layouts/Page"
+import Deck from "../src/models/Deck"
 
-export default () => (
-  <Page title="Poker Rules">
-    <p>To do.</p>
+export default () => {
+
+  const howManyCards = 5,
+    [deck, setDeck] = useState([]);
+
+  useEffect(() => {
+    startNewGame();
+  }, [])
+
+  const startNewGame = () => {
+    var newHand = new Deck().getNewHand(howManyCards);
+    setDeck(newHand);
+  }
+
+  return <Page title="Poker Rules">
+
+    <div className="col-12">
+      <div className="row">
+        {deck.map((card) => {
+          // imageName is always unique.
+          return <div className="col" key={card.imageName}><img className="img-fluid" src={"/images/" + card.imageName + ".png"} /></div>
+        })}
+      </div>
+    </div>
   </Page>
-)
+
+}
