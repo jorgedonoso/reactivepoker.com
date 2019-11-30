ad05ac9365912bde1f9a8f7640c756b4f5a16a05
Jorge Donoso <jdevcl@gmail.com>
2019-11-30 11:19:03 -0500

    Add hero hand

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Page from "../layouts/Page"
 
 export default () => (
   <Page title="Reactive Poker">
-    <p>Welcome to Reactive Poker!!!</p>
+    <p className="text-center lead">A collection of card game resources for beginners
+    <img src="/images/hero.png" className="img-fluid" /></p>
   </Page>
 )
