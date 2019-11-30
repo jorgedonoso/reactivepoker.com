06343f3b0857476cad9b5779077c2d6ad1ca478c
Jorge Donoso <jdevcl@gmail.com>
2019-11-30 11:58:59 -0500

    Add TOS text

--- pages/terms-of-service.js
+++ pages/terms-of-service.js
@@ -3,6 +3,16 @@ import Page from "../layouts/Page"
 
 export default () => (
   <Page title="Terms of Service">
-    <p>To do.</p>
+    <p>Please read these Terms of Service ("Terms", "Terms of Service") carefully before using the https://reactivepoker.com/ website (the "Service") operated by Reactive Poker ("us", "we", or "our"). Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service. By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.</p>
+    <h2>Links To Other Web Sites</h2>
+    <p>Our Service may contain links to third-party web sites or services that are not owned or controlled by Reactive Poker. Reactive Poker has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Reactive Poker shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services. We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.</p>
+    <h2>Termination</h2>
+    <p>We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms. All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+    <h2>Governing Law</h2>
+    <p>These Terms shall be governed and construed in accordance with the laws of Nevada, United States, without regard to its conflict of law provisions. Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
+    <h2>Warranty</h2>
+    <p>WE ENDEAVOUR TO PROVIDE THE BEST SERVICE WE CAN, BUT YOU UNDERSTAND AND AGREE THAT THIS WEBSITES'S SERVICE IS PROVIDED “AS IS” AND “AS AVAILABLE”, WITHOUT EXPRESS OR IMPLIED WARRANTY OR CONDITION OF ANY KIND. USE THIS WEBSITE AT YOUR OWN RISK.</p>
+    <h2>Changes</h2>
+    <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time with no warning. If you do not agree to this policy, please stop using the Service.</p>
   </Page>
 )
