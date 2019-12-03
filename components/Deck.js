e93b8a76da121b8dd249de709a364dafc36b129c
Jorge Donoso <jdevcl@gmail.com>
2019-12-02 19:44:52 -0500

    Display "Hold" under card if selected

--- components/Deck.js
+++ components/Deck.js
@@ -3,18 +3,21 @@ const Deck = ({ hand, clickEvent, selectedCards }) => {
     return <div className="row mb-4">
         {
             hand.map((card) => {
-                let extra = "pt-4";
+
+                let extra = "pt-4"; // To create "pull up" effect.
+                let holdMessage = "invisible"; // To toggle "Hold" text.
 
                 if (selectedCards && selectedCards.indexOf(card) > -1) {
-                    extra = "pb-4";
+                    extra = "";
+                    holdMessage = "font-weight-bold lead";
                 }
 
-                // imageName is always unique.
-                return <div className="col p-0 text-center"
-                    key={card.imageName}>
+                // "imageName" is always unique.
+                return <div className="col p-0 text-center" key={card.imageName}>
                     <img className={"img-fluid " + extra}
                         src={"/images/" + card.imageName + ".png"}
                         onClick={() => clickEvent && clickEvent(card)} />
+                    <p className={holdMessage}>Hold</p>
                 </div>
             })
         }
