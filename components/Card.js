18152cfa69873dd7b0e9788f0f060594a00d1718
Jorge Donoso <jdevcl@gmail.com>
2019-12-15 21:27:19 -0500

    Add unique key to prevent rendering caching

--- components/Card.js
+++ components/Card.js
@@ -7,11 +7,13 @@ const Card = ({ clickEvent, extraClasses, card, index }) => {
     return (card && card.imageName)
         ? <img className={"img-fluid animated fadeIn " + delay + " " + extraClasses}
             src={cardImages[card.key]}
-            onClick={() => clickEvent && clickEvent(card)} />
+            onClick={() => clickEvent && clickEvent(card)}
+            key={Date.now()} />
         :
         <img className={"img-fluid animated fadeIn " + delay + " " + extraClasses}
             src={cardImages["back"]}
-            onClick={() => clickEvent && clickEvent(card)} />
+            onClick={() => clickEvent && clickEvent(card)}
+            key={Date.now()} />
 }
 
 export default Card
