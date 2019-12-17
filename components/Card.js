a41a030229107a17b6f21a98bb0a665c3d766a0b
Jorge Donoso <jdevcl@gmail.com>
2019-12-16 23:07:16 -0500

    Code clean up

--- components/Card.js
+++ components/Card.js
@@ -3,17 +3,12 @@ import cardImages from "../src/cardImages"
 const Card = ({ clickEvent, extraClasses, card, index, deckId }) => {
 
     const delay = "delay-" + index;
+    const imgSrc = card ? cardImages[card.key] : cardImages["back"];
 
-    return (card && card.imageName)
-        ? <img className={"img-fluid animated fadeIn " + delay + " " + extraClasses}
-            src={cardImages[card.key]}
-            onClick={() => clickEvent && clickEvent(card)}
-            key={deckId} />
-        :
-        <img className={"img-fluid animated fadeIn " + delay + " " + extraClasses}
-            src={cardImages["back"]}
-            onClick={() => clickEvent && clickEvent(card)}
-            key={deckId} />
+    return <img className={"img-fluid animated fadeIn " + delay + " " + extraClasses}
+        src={imgSrc}
+        onClick={() => clickEvent && clickEvent(card)}
+        key={deckId} />
 }
 
 export default Card
