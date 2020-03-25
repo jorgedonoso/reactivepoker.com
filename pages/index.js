2680f232484ca66cc61bb671b372eb47f34aea08
Jorge Donoso <jdevcl@gmail.com>
2020-03-25 13:41:52 -0400

    Add CTAs to home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Page from "../layouts/Page"
 import heroImage from "../src/images/hero.png"
+import Link from "next/link"
+import useMenu from "../src/hooks/useMenu"
 
-export default () => (
-  <Page title="Reactive Poker">
+export default () => {
+
+  const { menuData } = useMenu();
+
+  return <Page title="Reactive Poker">
     <div className="row">
       <div className="col">
         <p className="text-center lead">
@@ -12,5 +17,18 @@ export default () => (
         </p>
       </div>
     </div>
+    <div className="row">
+      {
+        menuData.map((cta, $key) => {
+          if (!cta.excludeFromMobile) {
+            return <div className="col-md mb-2" key={$key}>
+              <Link prefetch={false} href={cta.path}>
+                <a className="btn btn-danger shadow py-3 btn-block text-uppercase">{cta.ctaLabel}</a>
+              </Link>
+            </div>
+          }
+        })
+      }
+    </div>
   </Page>
-)
+}     
--- src/hooks/useMenu.js
+++ src/hooks/useMenu.js
@@ -6,9 +6,9 @@ export default () => {
 
     useEffect(() => {
         setMenuData([
-            { label: "Poker Rules", path: "/poker-rules", icon: "book" },
-            { label: "Jacks or Better", path: "/jacks-or-better", icon: "map-signs" },
-            { label: "Card Counting", path: "/card-counting", icon: "balance-scale-right" },
+            { label: "Poker Rules", path: "/poker-rules", icon: "book", ctaLabel: "Practice Poker Rules" },
+            { label: "Jacks or Better", path: "/jacks-or-better", icon: "map-signs", ctaLabel: "Play Jacks or Better" },
+            { label: "Card Counting", path: "/card-counting", icon: "balance-scale-right", ctaLabel: "Practice Card Counting" },
             { label: "Terms of Service", path: "/terms-of-service", icon: "exclamation-triangle", excludeFromMobile: true }
         ])
     }, []);
